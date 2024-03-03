26aa32163b2343b28907b84dff763331c329f398
OB <ob@OBs-MacBook-Air.local>
2024-03-03 14:06:21 -0600

    ButtonAddition

--- lib/app/app_sign_in/sign_in_page.dart
+++ lib/app/app_sign_in/sign_in_page.dart
@@ -8,6 +8,7 @@ class SignInPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[100],
       appBar: AppBar(
         title: Text('Time Tracker'),
       ),
@@ -15,32 +16,48 @@ class SignInPage extends StatelessWidget {
     );
   }
 
-  Widget _buildContent(){
+  Widget _buildContent() {
     return Container(
-        color: Colors.yellow,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Container(
-              color: Colors.orange,
-              child: SizedBox(
-                height: 100,
+      padding: EdgeInsets.all(16.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Text(
+            'Sign In',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 32.0,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+          SizedBox(
+            height: 8.0,
+          ),
+          ElevatedButton(
+            onPressed: _signInWithGoogle,
+            style: ButtonStyle(
+              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+              ),
+              backgroundColor: MaterialStateProperty.all(Colors.white),
+            ),
+            child: Text(
+              'Google Sign In',
+              style: TextStyle(
+                color: Colors.black87,
+                fontSize: 15.0,
               ),
             ),
-            Container(
-              color: Colors.purple,
-              child: SizedBox(
-                height: 100,
-              ),
-            ),
-            Container(
-              color: Colors.green,
-              child: SizedBox(
-                height: 100,
-              ),
-            ),
-          ],
-        ),
-      );
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _signInWithGoogle() {
+    //TODO
   }
 }
